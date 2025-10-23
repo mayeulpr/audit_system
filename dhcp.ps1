14399237cd1e366546bbce6e3a2a61b3194575a5
mayeulpr <mayeul.prange@gmail.com>
2025-10-23 11:50:47 +0200

    admin

--- dhcp.ps1
+++ dhcp.ps1
@@ -12,6 +12,14 @@ Set-DhcpServerv4OptionValue -ScopeId 192.168.147.0 `
 -DnsDomain "entrepriseXY.fr" `
 -Router 192.168.147.1
 
+
+# Réservation d’un poste administratif
+Add-DhcpServerv4Reservation -ScopeId 192.168.147.0 `
+-IPAddress 192.168.147.205 `
+-ClientId "00-1A-2B-3C-4D-5E" `
+-Description "Poste administratif" `
+-Name "PC_Admin"
+
 # Vérifications
 Get-DhcpServerv4Scope
 Get-DhcpServerv4OptionValue -ScopeId 192.168.147.0
