c50658167905d8c560bcb4278ce66efa9b610876
mayeulpr <mayeul.prange@gmail.com>
2025-10-23 13:50:09 +0200

    admin

--- dns.ps1
+++ dns.ps1
@@ -3,3 +3,6 @@
 Add-DnsServerPrimaryZone -Name "entrepriseXY.fr" -ReplicationScope "Domain"
 Add-DnsServerResourceRecordA -Name "srv-dc1" -ZoneName "entrepriseXY.fr" -IPv4Address "192.168.147.10"
 Set-DnsServerForwarder -IPAddress 8.8.8.8,8.8.4.4
+
+Get-DnsServerZone | Select-Object ZoneName, ZoneType
+Resolve-DnsName "$RecordName.$ZoneName"
